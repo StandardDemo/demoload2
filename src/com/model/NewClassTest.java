5974fe53a40e191185929b240af2f1a7e55f0151
sujoyghosh26 <sghosh@kovair.com>
2019-08-05 21:31:21 -0700

    f

---
+++ src/com/model/NewClassTest.java
@@ -0,0 +1,36 @@
+package com.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NewClassTest {
+
+	
+	
+	@BeforeClass
+	public static void setUpBeforeClass()  {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass()  {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown()  {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/com/model/UserTest.java
@@ -0,0 +1,55 @@
+package com.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UserTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception { 
+	}
+
+	@AfterClass 
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testUser() {
+		fail("Not  yet implemented");
+	}
+
+	@Test
+	public void testGetUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserId() {
+		fail("Not yet   implemented"); 
+	}
+
+	@Test
+	public void testGetPassword() { 
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented"); 
+	}
+
+}
