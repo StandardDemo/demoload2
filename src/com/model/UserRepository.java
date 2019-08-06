ab0935ac4700f2fc97c1250b530df86c5515f5ef
sujoyghosh26 <sghosh@kovair.com>
2019-08-05 21:29:19 -0700

    demo

---
+++ src/com/model/UserRepository.java
@@ -0,0 +1,18 @@
+
+package com.model;
+import java.util.*;
+public class UserRepository {
+	 public static List<User>  userList= new ArrayList<User>();
+
+	public List<User> getAllUsers() {
+		return userList;
+	}
+
+	public void addUsersToRepository() {
+		User user1 = new User("admin", "admin");
+		User user2 = new User("ac", "ac");
+		userList.add(user1);
+		userList.add(user2);
+	} 
+//comment DP-
+}
